9873e7fc159b5577e3690d7531855ab3a531bbd3
devlawper <contact@devlawper.com>
2019-05-30 11:45:21 +0200

    Confirm sur suppression user

--- src/js/actions.js
+++ src/js/actions.js
@@ -2,9 +2,10 @@
 
 // Version AJAX de la suppresion d'un utilisateur du tableau
 function supprUser(){
-	$.post("php/suppr_user.php",{id:$(this).data('id')},valideSup);
+	if (confirm("Voulez-vous vraiment supprimer cet utilisateur ?")) {
+		$.post("php/suppr_user.php",{id:$(this).data('id')},valideSup);
+	}	
 }
-
 function valideSup(reponse){
 	$('.'+reponse).remove();
 }
